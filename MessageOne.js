670a177b1c59d15499e335832083a5a566bb7d96
MuhammadIlys <saylikhan97@gmail.com>
2023-09-27 10:58:01 +0500

    Import and Export

---
+++ MessageOne.js
@@ -0,0 +1,7 @@
+import MessageTwo from "./MessageTwo.js";
+import {name,age} from "./MessageTwo.js";
+
+console.log('I am message from MessageOne file');
+console.log('Message2 : ' + MessageTwo);
+console.log("Name : " + name);
+console.log("Age : " + age);
---
+++ MessageTwo.js
@@ -0,0 +1,8 @@
+
+var MessageTwo = "I am message from MessageTwo file";
+const Person = {
+    name : "Ali",
+    age : 23
+}
+export const { name, age } = Person;
+export default MessageTwo;
